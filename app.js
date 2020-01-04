9c1b627c21313178225465d62d8a93df6621554d
viniciusrplima <vinicius.rodrigues.lima@ccc.ufcg.edu.br>
2020-01-04 15:38:12 -0300

    Removendo dados privados do Firebase

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyC-LLiqUzzZRbEY6cXOSOYn_dJyt-RdmTY",
-    authDomain: "code-and-wrench.firebaseapp.com",
-    databaseURL: "https://code-and-wrench.firebaseio.com",
-    projectId: "code-and-wrench",
-    storageBucket: "code-and-wrench.appspot.com",
-    messagingSenderId: "570520386173",
-    appId: "1:570520386173:web:332e0d9a219547042b0f14",
-    measurementId: "G-3WNNE2XPWT"
-  };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
